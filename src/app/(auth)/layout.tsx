75e39d7bd22573f39d5683912c42be9473a6c0c3
Obinna Anosike <smartlify09@gmail.com>
2026-08-13 11:26:45 +0100

    fix logo path

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -12,7 +12,7 @@ export default function AuthLayout({
         <div className="mx-auto px-4 py-5 lg:max-w-7xl lg:px-25 2xl:max-w-360 2xl:px-50">
           <Link href="/" className="flex w-[157px] flex-col gap-1">
             <Image
-              src="/figma-home/logo.svg"
+              src="/figma-home/ma-logo.svg"
               alt="ModernAdvocates Inc."
               width={58}
               height={44}
